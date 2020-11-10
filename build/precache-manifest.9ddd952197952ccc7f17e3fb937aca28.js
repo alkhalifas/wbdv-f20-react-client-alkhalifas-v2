9b43431e189157fd7479d64c984aaa50cadf42b1
Saleh Alkhalifa <salehesam@gmail.com>
2020-11-09 20:51:19 -0500

    Made changes to deploy to Node.js

---
+++ build/precache-manifest.9ddd952197952ccc7f17e3fb937aca28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5e5abc3ffd72c9800e1938fda795d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f34c6485e649ea1989",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "89b7f68388e6fa816f20",
+    "url": "/static/css/main.e523bff3.chunk.css"
+  },
+  {
+    "revision": "e5f34c6485e649ea1989",
+    "url": "/static/js/2.2724ff7a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.2724ff7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b7f68388e6fa816f20",
+    "url": "/static/js/main.da220778.chunk.js"
+  },
+  {
+    "revision": "5e9942c7fd9cca4bd9e7",
+    "url": "/static/js/runtime-main.228b807b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
